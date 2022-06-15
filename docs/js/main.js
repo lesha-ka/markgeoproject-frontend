7856f4abb64420353b9fab36c67934c35f2e720f
aleksei.kazulin <Kazulin.job@gmail.com>
2022-06-16 01:21:31 +0300

    popup in docs

--- docs/js/main.js
+++ docs/js/main.js
@@ -1404,6 +1404,23 @@ var init = function init() {
   $('.js-popup-contacts-watch').on('click', function () {
     $('.popup-contacts').fadeIn();
   });
+  $(document).on('click', '.js-popupOpen', function () {
+    var popupId = $(this).data('popup');
+    var popup = $('[data-popup-number="' + popupId + '"]');
+    popup.fadeIn(0);
+  });
+  $(document).on('click', '.js-popupClose', function () {
+    var popup = $('.popup');
+    popup.fadeOut(0);
+  });
+  $(document).ready(function () {
+    if ($('.js-popup').length) {
+      $(document).on('keydown', function (e) {
+        var popup = $('.js-popup');
+        if (e.keyCode == 27) popup.fadeOut(0);
+      });
+    }
+  });
 };
 
 /* harmony default export */ __webpack_exports__["default"] = ({
