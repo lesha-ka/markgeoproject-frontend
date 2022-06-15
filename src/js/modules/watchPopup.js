ac8a472aa6d72625edf14f49982f9564add917c2
aleksei.kazulin <Kazulin.job@gmail.com>
2022-06-16 01:17:58 +0300

    popup for review

--- src/js/modules/watchPopup.js
+++ src/js/modules/watchPopup.js
@@ -5,6 +5,24 @@ const init = () => {
   $('.js-popup-contacts-watch').on('click', function () {
   	$('.popup-contacts').fadeIn();
   });
+  $(document).on('click', '.js-popupOpen', function(){
+      let popupId = $(this).data('popup');
+      let popup = $('[data-popup-number="'+popupId+'"]');
+      popup.fadeIn(0);
+  });
+  $(document).on('click', '.js-popupClose', function(){
+    let popup = $('.popup');
+    popup.fadeOut(0);
+  });
+  $(document).ready(function(){
+    if($('.js-popup').length) {
+      $(document).on('keydown', function(e) {
+        let popup= $('.js-popup');
+        if (e.keyCode == 27)
+        popup.fadeOut(0);
+      });
+    }
+  });
 };
 
 export default {
